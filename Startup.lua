c748f71db066a9dd2af2c939882ed205127769b0
apaza610 <homar6@me.com>
2024-03-04 07:55:56 -0400

    Launch LuaMacros using a button in DoubleCommander

---
+++ Startup.lua
@@ -0,0 +1,5 @@
+lmc.minimizeToTray = true
+lmc_minimize()
+lmc_send_keys('%{F4}')
+--lmc_load('C:\\Descargas\\luamacros\\Main.lua')--
+lmc_load('E:\\misapps\\AutomateWindowsOS\\ExtraNumpad.lua')
---
+++ lanzador.bat
@@ -0,0 +1,3 @@
+cd /e E:\Progs\gral\luamacros
+LuaMacros.exe -r "E:\misapps\AutomateWindowsOS\Startup.lua"
+exit
